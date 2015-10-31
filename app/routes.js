46380c61b66f9c308361ef543e194f71ec2a78c2
Ramon Jose Arellano Bitrian <ramonmoxo@gmail.com>
2015-10-31 20:55:03 +0100

    User model (endpoint not working)

--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,33 @@
  * Created by ramon on 31/10/2015.
  */
 var express  = require('express');
+var User     = require('./user-model');
 
+/* API ROUTES */
+var apiRoutes = express.Router();
+
+apiRoutes.get("/", function(req, res) {
+    res.json("Welcom to the coolest API on earth");
+});
+
+apiRoutes.route("/users")
+    .get(function(req, res){
+        User.find({}, function(err, users){
+            res.send.json(users);
+        });
+    })
+    .post(function(req, res){
+        new User({
+            username: req.body.username,
+            password: req.body.password
+        }).save(function(err, data){
+            if (err) res.status(500).send(err);
+            else res.json(data);
+        });
+    });
 
 var routes = express.Router();
+routes.use("/api", apiRoutes);
 routes.get('*', function(req, res) {
     res.sendFile(__dirname + '../public/index.html');
 });
