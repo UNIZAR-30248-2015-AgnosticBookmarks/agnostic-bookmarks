667ee6907dea2eef269c0740903f6ea2c045dd2f
Adrián Moreno <admoreno@outlook.com>
2015-11-01 01:01:21 +0100

    User endpoints working

--- app/routes.js
+++ app/routes.js
@@ -1,22 +1,28 @@
-/**
- * Created by ramon on 31/10/2015.
- */
-var express  = require('express');
-var User     = require('./user-model');
+// =============================================================================
+//  This file defines the endpoints for the RESTFul API and the routes for the
+//  application.
+// =============================================================================
+
+var express = require('express');
+var User    = require('./user-model');
 
 /* API ROUTES */
 var apiRoutes = express.Router();
 
-apiRoutes.get("/", function(req, res) {
-    res.json("Welcom to the coolest API on earth");
+// API entry point
+apiRoutes.get('/', function(req, res) {
+    res.json('Welcome to the coolest API this side of the Mississippi :D');
 });
 
-apiRoutes.route("/users")
+//Users endpoint
+apiRoutes.route('/users')
+    // Get a list with all the users
     .get(function(req, res){
         User.find({}, function(err, users){
-            res.send.json(users);
+            res.json(users);
         });
     })
+    // Add a new user
     .post(function(req, res){
         new User({
             username: req.body.username,
@@ -27,8 +33,11 @@ apiRoutes.route("/users")
         });
     });
 
+/* GLOBAL ROUTES */
+// API endpoints go under '/api' route. Other routes are redirected to
+// index.html where AngularJS will handle frontend routes.
 var routes = express.Router();
-routes.use("/api", apiRoutes);
+routes.use('/api', apiRoutes);
 routes.get('*', function(req, res) {
     res.sendFile(__dirname + '../public/index.html');
 });
