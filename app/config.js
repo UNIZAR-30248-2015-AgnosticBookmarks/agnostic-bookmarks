d9d44fb9e291cefb18b0779640d4ccce73afa8c4
Adrián Moreno <admoreno@outlook.com>
2015-11-03 22:33:52 +0000

    Removed config.js from repo

--- app/config.js
+++
@@ -1,7 +0,0 @@
-// =============================================================================
-//  This file contains configuration parameters that will be used by the app.
-// =============================================================================
-
-module.exports = {
-    database: 'mongodb://localhost/agnostic-bookmarks' // Database location
-}
