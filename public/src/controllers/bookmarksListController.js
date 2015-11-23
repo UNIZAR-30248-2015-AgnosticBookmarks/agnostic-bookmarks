70ce7bc614b225068b53321a839650a5cf8c9015
Alberto Revel <alberto.revel@gmail.com>
2015-11-23 01:34:34 +0100

    List try....v2

--- public/src/controllers/bookmarksListController.js
+++ public/src/controllers/bookmarksListController.js
@@ -1,6 +1,7 @@
 
 var app = angular.module('AgnosticBookmarks');
 app.controller('bookmarksListCtrl', function($scope, $state, BookmarksService) {
+    $scope.showList = false;
     $scope.getList = function() {
         BookmarksService.bookmarksList($scope.user.name, onListResponse);
     };
