631215fa6af88b2b6478f843e447cd81ebd1678b
Alberto Revel <alberto.revel@gmail.com>
2015-11-23 22:30:37 +0100

    change state if registered

--- public/src/controllers/accessController.js
+++ public/src/controllers/accessController.js
@@ -2,13 +2,14 @@
 var app = angular.module('AgnosticBookmarks');
 app.controller('accessCtrl', function($scope, $state, UserService) {
     $scope.showError = false;
-    $scope.registerOK = false;
     $scope.registerError = false;
     $scope.enter = function() {
+		$scope.showError = false;
         UserService.authenticate($scope.user.name, $scope.user.password, onEnterResponse);
         //$scope.user = {name:pepe, password:"locolo"};
     };
     $scope.register = function() {
+		$scope.registerError = false;
         UserService.register($scope.user.name, $scope.user.password, onRegisterResponse);
     };
 
@@ -18,7 +19,7 @@ app.controller('accessCtrl', function($scope, $state, UserService) {
     }
 
     var onRegisterResponse = function (result) {
-        if (result) $scope.registerOK = true;
+        if (result) $state.go('home');
         else $scope.registerError = true;
     }
 
